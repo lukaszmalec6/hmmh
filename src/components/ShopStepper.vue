577d771af461a976be9975036192d92b6b2a11cf
Lukasz Malec <lukaszmalec6@gmail.com>
2022-07-19 14:10:59 +0200

    2nd step basic

--- src/components/ShopStepper.vue
+++ src/components/ShopStepper.vue
@@ -5,18 +5,12 @@
         <v-img src="../assets/tshirt-blank.png" width="345" height="170">
           <v-row class="tshirt-overlay px-5">
             <v-col cols="6" class="d-flex justify-center align-center">
-              <img v-if="formData.positionFront" src="https://picsum.photos/seed/piscum/50/"
-                class="tshirt-pic-front"><img>
-
+              <img v-if="formData.positionFront" :src="getPicUrl()" class="tshirt-pic-front" />
             </v-col>
-
             <v-col cols="6" class="d-flex justify-center align-center pl-6">
-              <img v-if="formData.positionBack" src="https://picsum.photos/seed/piscum/50/"
-                class="tshirt-pic-back"><img>
-
+              <img v-if="formData.positionBack" :src="getPicUrl()" class="tshirt-pic-back" />
             </v-col>
           </v-row>
-
         </v-img>
       </v-col>
     </v-row>
@@ -34,7 +28,7 @@
                 <v-checkbox v-model="formData.positionFront" label="Przód" class="pl-2"></v-checkbox>
                 <v-checkbox v-model="formData.positionBack" label="Tył" class="pl-2"></v-checkbox>
                 <v-btn @click="nextStep()" small color="primary" class="align-self-center">
-                  Continue
+                  Dalej
                 </v-btn>
               </v-col>
             </v-row>
@@ -46,13 +40,42 @@
           </v-stepper-step>
 
           <v-stepper-content step="2">
-            <v-card></v-card>
-            <v-btn @click="nextStep()">
-              Continue
-            </v-btn>
-            <v-btn @click="prevStep()">
-              Cancel
-            </v-btn>
+            <v-row>
+              <v-col cols="4" class="d-flex flex-column justify-center align-center">
+                <v-btn @click="prevPic()" class="mx-2" fab small outlined>
+                  <v-icon>
+                    mdi-arrow-left
+                  </v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="4" class="d-flex flex-column">
+                <img v-if="formData.positionFront" :src="getPicUrl()" class="pic-select-prev" />
+              </v-col>
+              <v-col cols="4" class="d-flex justify-center align-center">
+                <v-btn @click="nextPic()" class="mx-2" fab small outlined>
+                  <v-icon>
+                    mdi-arrow-right
+                  </v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" class="d-flex justify-center align-center mb-3">
+                <v-btn @click="randomPic()" small outlined class="align-self-center">
+                  Wylosuj zdjecie
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" class="d-flex justify-center align-center mb-5">
+                <v-btn @click="prevStep()" small outlined class="align-self-center mx-1">
+                  Wstecz
+                </v-btn>
+                <v-btn @click="nextStep()" small color="primary" class="align-self-center mx-1">
+                  Dalej
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-stepper-content>
 
           <!-- step 3 -->
@@ -83,9 +106,30 @@ export default {
 
   data: () => ({
     currentStep: 1,
+    picId: 1000,
     formData: {
       positionFront: true,
       positionBack: false,
+    },
+
+
+    randomPic() {
+      const randomId = Math.floor(Math.random() * (1025 - 1000) + 1000);
+      if (randomId === this.picId) {
+        this.picId = randomId + 1;
+      } else {
+        this.picId = randomId;
+      }
+
+    },
+
+    nextPic() {
+      this.picId = this.picId + 1;
+    },
+
+    prevPic() {
+      this.picId = this.picId - 1;
+
     },
 
     nextStep() {
@@ -94,6 +138,10 @@ export default {
 
     prevStep() {
       this.currentStep = this.currentStep - 1;
+    },
+
+    getPicUrl() {
+      return `https://picsum.photos/id/${this.picId}/50/`
     }
 
   }),
